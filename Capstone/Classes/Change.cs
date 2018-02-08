7619059c2db4b6c5ae11131ef92643c56b61e95c
John Kenny <jkennytech@gmail.com>
2018-02-08 13:38:53 -0500

    Worked on Change Class

--- Capstone/Classes/Change.cs
+++ Capstone/Classes/Change.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    class Change
+    public class Change
     {
+        public int Nickels { get; }
+        public int Dimes { get; }
+        public int Quarters { get; }
+        public decimal Total { get; }
+
+        public Change(decimal total)
+        {
+            
+            Total = total;
+            total = total * 100;
+            if (total % 25 == 0)
+            {
+                // quarters = total / 25;
+            }
+            else if (total % 25 > 0)
+            {
+                // quarters = (total / 25);
+                // total = total - (25 * quarters);
+                if (total % 10 == 0)
+                {
+                    // dimes = total / 10;
+                }
+                else if (total % 10 > 0)
+                {
+                    // dimes = total / 10;
+                    // total = total - (10 * dimes);
+                    // nickels = total / 5;
+                }
+            }
+
+
+
+        }
+
+
     }
 }
