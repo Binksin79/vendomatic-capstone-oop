7554d617915bf4db62bdbb94f438df30d14ed40c
Rob Roy <r.m.royiii@gmail.com>
2018-02-08 10:17:20 -0500

    1st Push. Created some classes, filled out VendingItemClass

---
+++ Capstone/Classes/VendingItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public class Vending_Item
+    {
+        public string ItemName { get; }
+        public int ItemPrice { get; }
+        public string ItemSlot { get; }
+        public string ItemType { get; }
+    }
+}
