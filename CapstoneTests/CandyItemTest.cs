51c9db71fe941b5999edc119c2c2e8c310fccb07
Rob Roy <r.m.royiii@gmail.com>
2018-02-10 11:46:42 -0500

    Added More Unit Tests/Still working code bloat

---
+++ CapstoneTests/CandyItemTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class CandyItemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
